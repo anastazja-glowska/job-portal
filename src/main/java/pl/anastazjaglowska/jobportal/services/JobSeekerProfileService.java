c8966bcd695f83b9cc02f9855ef24219418b13da
Anastazja Glowska <anastazjaglowska12345@gmail.com>
2025-07-23 21:48:00 +0200

    New classes related with Job Seeker

---
+++ src/main/java/pl/anastazjaglowska/jobportal/services/JobSeekerProfileService.java
@@ -0,0 +1,24 @@
+package pl.anastazjaglowska.jobportal.services;
+
+
+import org.springframework.stereotype.Service;
+import pl.anastazjaglowska.jobportal.entity.JobSeekerProfile;
+import pl.anastazjaglowska.jobportal.repository.JobSeekerProfileRepository;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+
+        return jobSeekerProfileRepository.findById(id);
+    }
+}
--- src/test/java/pl/anastazjaglowska/jobportal/controller/UsersControllerTestcontainersTest.java
+++ src/test/java/pl/anastazjaglowska/jobportal/controller/UsersControllerTestcontainersTest.java
@@ -1,75 +1,131 @@
 package pl.anastazjaglowska.jobportal.controller;
-//
-//import org.junit.jupiter.api.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.testcontainers.containers.MySQLContainer;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import pl.anastazjaglowska.jobportal.entity.Users;
-//import pl.anastazjaglowska.jobportal.entity.UsersType;
-//import pl.anastazjaglowska.jobportal.repository.UsersTypeRepository;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Testcontainers
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@AutoConfigureMockMvc
-//public class UsersControllerTestcontainersTest {
-//
-//    @ServiceConnection
-//    private static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8");
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Autowired
-//    UsersTypeRepository usersTypeRepository;
-//
-//
-//    static {
-//        mySQLContainer.start();
-//    }
-//
-//    @BeforeAll
-//    void setup() {
-//        UsersType recruiter = new UsersType(1, "Recruiter", new Users());
-//        UsersType jobSeeker = new UsersType(1, "Recruiter", new Users());
-//        usersTypeRepository.findAll();
-//    }
-//
-//
-//    @Order(1)
-//    @Test
-//    void testContainerIsRunning() {
-//        assertTrue(mySQLContainer.isRunning(), "Mysql container is not running");
-//        assertTrue(mySQLContainer.isCreated(), "Mysql container is not created");
-//    }
-//
-//    @Test
-//    @Order(2)
-//    void testRegister_whenValidDetailsProvided_returnRegisterForm(){
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
 
+import jakarta.validation.Valid;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.anastazjaglowska.jobportal.entity.Users;
+import pl.anastazjaglowska.jobportal.entity.UsersType;
+import pl.anastazjaglowska.jobportal.repository.UsersTypeRepository;
+import pl.anastazjaglowska.jobportal.services.UsersService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
+public class UsersControllerTestcontainersTest {
+
+    @ServiceConnection
+    private static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsersTypeRepository usersTypeRepository;
+
+    @Autowired
+    private UsersService usersService;
+
+    private Users users;
+    private UsersType recruiter;
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @BeforeEach
+    void setup() {
+
+        usersTypeRepository.deleteAll();
+
+
+        recruiter = new UsersType();
+        recruiter.setUserTypeName("Recruiter");
+        recruiter = usersTypeRepository.save(recruiter);
+
+
+        users = new Users();
+        users.setEmail("test@test.com");
+        users.setPassword("12345678");
+        users.setActive(true);
+        users.setRegistrationDate(java.sql.Date.valueOf(LocalDate.of(2025, 7, 23)));
+        users.setUserTypeId(recruiter);
+    }
+
+    @Order(1)
+    @Test
+    void testContainerIsRunning() {
+        assertTrue(mySQLContainer.isRunning(), "MySQL container is not running");
+        assertTrue(mySQLContainer.isCreated(), "MySQL container is not created");
+    }
+
+    @Order(2)
+    @Test
+    void testRegister_whenValidDetailsProvided_returnRegisterForm() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(model().attributeExists("getAllTypes"))
+                .andExpect(model().attributeExists("user"));
+    }
+
+//    @Order(3)
+//    @Test
+//    void testUserRegistration_whenValidDetailsProvided_returnsCorrectModel() throws Exception {
+//        mockMvc.perform(post("/register/new")
+//                        .param("email", users.getEmail())
+//                        .param("password", users.getPassword())
+//                        .param("isActive", String.valueOf(users.isActive()))
+//                        .param("registerDate", users.getRegistrationDate().toString())
+//                        .param("usersType.id", String.valueOf(recruiter.getUserTypeId()))
+//                )
+//                .andExpect(view().name("register"))
+//                .andExpect(model().attributeExists("getAllTypes"))
+//                .andExpect(model().attributeExists("user"));
+//    }
+}
+
+
+
+//@PostMapping("/register/new")
+//public String userRegistration(@Valid Users users, Model model) {
+//
+//    Optional<Users> optionalUsers = usersService.getUserByEmail(users.getEmail());
+//
+//    if(optionalUsers.isPresent()) {
+//        model.addAttribute("error",
+//                "Email already existed, try to login or register with other email.");
+//        List<UsersType> usersTypes = usersTypeService.getAll();
+//        model.addAttribute("getAllTypes", usersTypes);
+//        model.addAttribute("user",new Users());
+//        return "register";
+//    }
 
 //
 //@GetMapping("/register")
